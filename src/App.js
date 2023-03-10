386227cddde40a42c12c5671da6c51d357716a0a
Ken Liu <ken.liu1@ucalgary.ca>
2023-03-09 21:29:26 -0700

    Added navigate to import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Layout from './Layout';
 import UnselectedNote from './UnselectedNote'
 import SelectedNote from './SelectedNote';
